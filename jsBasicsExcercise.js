eb97e9bc943d5501f39f830f78b010815cc84d8d
zachmann00 <zach_mann@hotmail.com>
2021-02-22 14:21:46 -0500

    assignment complete

---
+++ jsBasicsExcercise.js
@@ -0,0 +1,33 @@
+// 1
+console.log(44);
+// 2
+//Primitive, Strings, Booleans, Null, Undefined
+// 3
+// Adding, Subtracting, Multiplying, Dividing, Exponents, Modulus
+// 4
+console.log(5%4);
+console.log(5%2);
+// 5
+console.log('I’M MARY POPPINS, Y’ALL!');
+// 6
+console.log("Wanda" + "Vision");
+// 7
+console.log("Zach".length + "Mann".length);
+// 8
+console.log("Avengers: Endgame"[16]);
+// BONUS
+// 1
+console.log("\\");
+// 2
+console.log(5**4-9);
+// 3
+console.log("I".length % "AM".length % "IRON".length % "MAN".length -1);
+// 4
+console.log("CAP"[1]);
+console.log("VISION"[0]);
+console.log("HAWKEYE"[4]);
+console.log("NICK FURY"[0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR"[3]);
+console.log("STAR-LORD"[0]);
